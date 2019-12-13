1090a395e4e7fbdfa64be56dc251946b304bfb2c
hajarbouhamyd <38794458+hajarbouhamyd@users.noreply.github.com>
2019-12-13 15:27:49 +0100

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,29 @@
+package com.bank.demo.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    @OneToOne
+    private Login login;
+    private Long phone_number;
+    private String dateNaissance;
+    @OneToMany(mappedBy = "client")
+    private List<Compte> comptes;
+    @OneToOne(mappedBy = "client")
+    private List<Message> messages;
+
+}
